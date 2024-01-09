cc8ddf9abb64bde40534390ca22ce5c2e06e5874
Amurie666 <143251334+Amurie666@users.noreply.github.com>
2024-01-09 09:58:39 +0100

    Create LinearX.h

---
+++ LinearX/LinearX.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<set>
+#include<cmath>
+#include<algorithm>
+namespace LX {
+#include "Matrix.h"
+#include "Tuple.h"
+#include "Polynomial.h"
+#include "Complex.h"
+#include "Modulo.h"
+#include "QuotientSpace.h"
+#include "Cartesian_product.h"
+#include "Methods.h"
+};
