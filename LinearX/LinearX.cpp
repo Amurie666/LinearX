bc9b59ea81e89862605f05cf7939b5c6fb386399
Amurie666 <143251334+Amurie666@users.noreply.github.com>
2024-01-09 09:57:43 +0100

    Update LinearX.cpp

--- LinearX/LinearX.cpp
+++ LinearX/LinearX.cpp
@@ -1,76 +1,30 @@
-#include <iostream>
-#include "Matrix.h"
-#include "Tuple.h"
-#include "Polynomial.h"
-#include "Complex.h"
-#include "Modulo.h"
-#include "QuotientSpace.h"
-#include "Cartesian_product.h"
+#include "LinearX.h"
 
 int main() {
-    //Usage...
+    LX::Matrix<LX::Complex> A = { 
+        {LX::make_complex(15,10), LX::make_complex(4.8,-7), LX::make_complex(3,0)},
+        {LX::make_complex(-3,0), LX::make_complex(0,-4), LX::make_complex(-2, -2)},
+        {LX::make_complex(3,-7), LX::make_complex(0,-9), LX::make_complex(-2,8)}
+    };
 
-    //Matrices...
-    Matrix<int> A(2, 3), B(3, 3);
-    Matrix<int> F(2, 3);
-    Matrix<Complex> W(2, 2);                    
-    A = { {1, 2, 3}, {4, 5, 6}, {1, 2, 3} };    //This will change the dimensions of matrix A.
-    B = { {1, 2, 3}, {4, 5, 6}, {4, 5, 6} };
-    F = std::move(A);
-    std::cout << F * 2 << std::endl;
-    std::cout << 2 * F << std::endl;
-    std::cout << F + B << std::endl;
-    std::cout << F.transpose().trace() << std::endl;
-
-    //Tuples...
-    Tuple<int> t1(3), t2(3);
-    t1 = { 1, 2, 3 };
-    t2 = { 1, 1, 1 };
-    Tuple<int> t3 = std::move(t1);
-    Tuple<int> t4(t2);
-    std::cout << t3 + t4 << std::endl;
-    std::cout << 2 * t4 << "   " << t4 * 2 << std::endl;
-
-    //Polynomials...
-    Polynomial<int> p1(3);         
-    Polynomial<int> p2(4);          
-    Polynomial<int> p3(4);
-    p3 = std::move(p2);
-    p1 = { 1, 2, 3 };
-    p3 = { 1, 1, 1, 1 };
-    p3 = { 2, 1, 1, 3, 1, 1, 1 };      //This will generate the polynomial: (2x^4 + x^3 + x^2 + 3x +1) + 1 + 1 = (2x^4 + x^3 + x^2 + 3x + 3)
-    std::cout << p3 << std::endl;
-    std::cout << "+      " << p1 << std::endl;
-    std::cout << "= " << p3 + p1 << std::endl;
+    LX::Matrix<LX::Complex> b = { 
+        {LX::make_complex(5,8)},
+        {LX::make_complex(0,-1)},
+        {LX::make_complex(2,0)}
+    };
+    std::cout << "\nSolving the system:\n" << A << "=" << b << "\nSolution:\n" << LX::SYSTEM_SOLVE(A, b);
     
-    //Complex numbers...
-    Complex y(3, 4);
-    Complex x(1, 2);    //1+2i
-    Complex x_0(3, 4);
-    Complex x_1(4, 4);
-    Complex x_2(5, 4);
-    Complex z = std::move(y);
-    Complex w(1, 2);
-    std::cout << w / z << "    " << w * z << "    " << w + z << std::endl;
+    LX::Matrix<double> A_0 = {
+        {3, -7, 4 },
+        {1, -2, 1},
+        {2, -1, -2}
+    };
 
-    //Numbers in Z_7...
-    Zmod<7> n(3);
-    Zmod<7> m(5);
-    std::cout << n * m << "     " << n / m << "       " << n + m;
+    LX::Matrix<double> b_0 = {
+        {10},
+        {3},
+        {6}
+    };
+    std::cout << "\nSolving the system:\n" << A_0 << "=" << b_0 << "\nSolution:\n" << LX::SYSTEM_SOLVE(A_0, b_0);
 
-
-    //QuotientSpace and subspaces...
-    Matrix<Complex> K = { {x_0, x}, {x_0, x} };
-    Matrix<Complex> L = { {x, x_1}, {x, x_0} };
-    Matrix<Complex> M = { {x_1, x_0}, {x, x} };
-    Matrix<Complex> N = { {x_2, x}, {x_0, x_0} };
-
-
-    //You can skip this part for now... :
-    //Subspaces and Cosets...
-    std::set<Matrix<Complex>> subspace = { K, L, M, N };    
-    QuotientSpace<Complex, Matrix> QS(subspace, N);         //Initialize the subspace along with the coset: (subspace + N)
-    auto newcoset = QS + L;
-    auto somecoset = newcoset + (QS + M);                   //somecoset = QS + (L+M)
-    return 0;
 }
