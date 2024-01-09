638b728a9a5ef2047e5abe3b510b645128b5bd17
Amurie666 <143251334+Amurie666@users.noreply.github.com>
2024-01-09 10:00:46 +0100

    Update Matrix.h

--- LinearX/Matrix.h
+++ LinearX/Matrix.h
@@ -1,5 +1,4 @@
 #pragma once
-#include<vector>
 template<typename field>
 class Matrix {
 protected:
@@ -11,6 +10,7 @@ public:
     //Constructors
     Matrix() : rows(0), columns(0) {}
     Matrix(size_t n, size_t m) : rows(n), columns(m), entries(n, std::vector<field>(m)) {}
+    Matrix(size_t n, std::vector<field> v) : rows(n), columns(v.size()), entries(n, v) {}
     Matrix(const Matrix& other) : rows(other.rows), columns(other.columns), entries(other.entries) {}
     Matrix(Matrix&& other) noexcept : rows(other.rows), columns(other.columns), entries(std::move(other.entries)) {}
     Matrix(std::initializer_list<std::initializer_list<field>> entryini)
@@ -22,6 +22,21 @@ public:
             entries[i] = row;
             i++;
         }
+    }
+    size_t size() {
+        if (rows == columns)
+            return entries.size();
+        return 0;
+    }
+    size_t max_column(size_t j) {
+        std::vector<field> temp;
+        for (size_t i{ 0 }; i < rows; i++) {
+            temp.push_back(((*this)[i])[j]);
+        }
+        auto max = std::max_element(temp.begin(), temp.end(), [](field a, field b) {return abs(a) < abs(b); });
+        if (max != temp.end())
+            return std::distance(temp.begin(), max);
+        return -1;
     }
     //Assignment operators
     Matrix<field>& operator=(Matrix&& other) noexcept {
@@ -80,7 +95,17 @@ public:
 
         return result;
     }
-
+    Matrix<field> operator*(const Matrix<field>& rhs) const {
+        if (columns != rhs.rows) throw std::invalid_argument("Error: Cannot perform multiplication.");
+        Matrix<field> result(rows, rhs.columns);
+        for (size_t i{ 0 }; i < rows; i++)
+            for (size_t j{ 0 }; j < rhs.columns; j++) {
+                result.entries[i][j] = 0;
+                for (size_t k{ 0 }; k < rhs.rows; k++)
+                    result.entries[i][j] += entries[i][k] * rhs.entries[k][j];
+            }
+        return result;   
+    }
     //Method to compute the trace of a square matrix (i.e the sum of diagonal entries)
     field trace() const {
         field trace{};
@@ -99,6 +124,11 @@ public:
                 trans.entries[i][j] = entries[j][i];
         return trans;
     }
+   
+    typename std::vector<std::vector<field>>::iterator begin() { return entries.begin(); }
+    typename std::vector<std::vector<field>>::iterator end() { return entries.end(); }
+
+    inline std::vector<field>& operator[](size_t s) { return entries[s]; }
     //Overloaded Matrix-scalar multiplication.
     friend Matrix<field> operator*(const field& constant, const Matrix<field>& other) {
         Matrix result(other.rows, other.columns);
@@ -122,6 +152,62 @@ public:
         }
         return out;
     }
+    Matrix<field> exclude() {
+
+    }
+    void erase_column(typename std::vector<field>::iterator it) {
+        for (const auto& elem : entries) {
+            elem.erase(it);
+        }
+        columns--;
+    }
+    void erase_column(size_t i) { 
+        for (auto& elem : entries) {
+            elem.erase(elem.begin() + i);
+        }
+        columns--; 
+    }
+    void erase_row(size_t i) { entries.erase(begin()+i); rows--; }
+    void erase_row(typename std::vector<std::vector<field>>::iterator it) { entries.erase(it); rows--; }
+    field det() {
+        if (rows != columns)
+            throw std::invalid_argument("Not a square matrix.");
+        //Base case.
+        if (rows == 2)
+            return entries[0][0] * entries[1][1] - entries[0][1] * entries[1][0];
+        if (rows == 1)
+            return entries[0][0];
+        //Recursion
+        field sum{};
+        for (size_t j{ 0 }; j < columns; j++) {
+            Matrix<field> temp = *this;
+            temp.erase_row(0);
+            temp.erase_column(j);
+            sum += ((j % 2) == 0) ? entries[0][j] * temp.det() : static_cast<field>(-1) * entries[0][j] * temp.det();
+        }
+        return sum;
+    }
+    bool is_lower() {
+        if (rows != columns)
+            return false;
+        for (size_t i{ 0 }; i < rows; i++)
+            for (size_t j{ i + 1 }; j < columns; j++)
+                if (entries[i][j] != 0)
+                    return false;
+        return true; 
+    }
+    bool is_upper() {
+        if (rows != columns)
+            return false;
+        for (size_t i{ 0 }; i < rows; i++)
+            for (size_t j{ 0 }; j < i; j++)
+                if (entries[i][j] != static_cast<field>(0))
+                    return false;
+        return true;
+    }
+    size_t nrows() { auto r = rows;  return r; }
+    size_t ncolumns() { auto n = columns; return n; }
+
 
     //Comparision operators... (Unimportant for now...)
     bool operator==(const Matrix& other) const {
@@ -136,3 +222,4 @@ public:
     }
 
 };
+
